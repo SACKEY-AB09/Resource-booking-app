3b80b943e4c415a4692efcf950ccf89bc39850e1
sackey <sackeyabraham67@gmail.com>
2026-03-23 09:55:21 +0000

    Found the headers of the conflict and resolved it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 // App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Intropage from "./Pages/Intropage";
-import UserHomepage from "./Pages/userHomepage";
-import AuthStack from "../Stacks/AuthStack";
-import AdminStack from "../Stacks/AdminStack";
-import Booking from "./Pages/Booking.jsx";
-import ResourceAvailable from "./Admin/resourceavailable.jsx";
-import MyBookings from "./Pages/MyBookings.jsx";
-import BookResource from "./Pages/bookResource.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Intropage from './Pages/Intropage'
+import UserHomepage from './Pages/userHomepage'
+import AuthStack from '../Stacks/AuthStack'
+import AdminStack from '../Stacks/AdminStack'
+import Booking from './Pages/Booking.jsx'
+import ResourceAvailable from './Admin/resourceavailable.jsx'
 
 function App() {
   return (
@@ -17,19 +15,16 @@ function App() {
         <Route path="/login" element={<AuthStack />} />
         <Route path="/signup" element={<AuthStack />} />
         <Route path="/home" element={<UserHomepage />} />
-        <Route path="/resource/:id" element={<Booking />} />
+        <Route path="/resource/:id" element={<Booking />} />  
         <Route path="/admin/*" element={<AdminStack />} />
-<<<<<<< HEAD
         <Route path="/resources" element={<ResourceAvailable />} /> 
         <Route path ="/resource/:id" element = {<Booking isAdmin ={true} />} />
-=======
-        <Route path="/resources" element={<ResourceAvailable />} />
         <Route path="/mybookings/" element={<MyBookings />} />
         <Route path="/mybookings/book" element={<BookResource />} />
->>>>>>> 1effbc9648ecf4fc2be27f83ae6bc3dc4494db95
+
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
